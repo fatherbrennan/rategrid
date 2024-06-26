ff77f02368c120c8240fcaf60947767c6b0b3656
fatherbrennan <bjt.lepore@gmail.com>
2024-06-26 11:19:07 +0930

    fix: :bug: fix required class prop to be optional

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,3 @@
 import type { PropsOf as QwikPropsOf } from '@builder.io/qwik';
 
-export type PropsOf<T> = Omit<QwikPropsOf<T>, 'class'> & { class: string | undefined | null | false };
+export type PropsOf<T> = Omit<QwikPropsOf<T>, 'class'> & { class?: string | undefined | null | false };
