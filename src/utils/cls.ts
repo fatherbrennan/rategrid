02bae65179bbe1313b04a13eb7d20f9a977bc34c
fatherbrennan <bjt.lepore@gmail.com>
2024-07-10 09:20:37 +0930

    feat: :technologist: add cls utility to easily update element class string

---
+++ src/utils/cls.ts
@@ -0,0 +1,8 @@
+export const cls = (...args: (string | boolean | undefined | null)[]): string => {
+  let classes = '';
+  for (let i = 0; i < args.length; i++) {
+    const potentialClass = args[i];
+    potentialClass && (classes += potentialClass + ' ');
+  }
+  return classes.slice(0, -1);
+};
