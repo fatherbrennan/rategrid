eb3df7fb3496e51575d9886c76b9f4233ced6387
fatherbrennan <69226613+fatherbrennan@users.noreply.github.com>
2025-04-03 17:28:20 +1030

    Fix/fix navigation issues (#6)
    * ci: :hammer: add serve script
    
    * fix: :bug: fix navigation issues
    
    * chore: :white_check_mark: add hasTrailingQuestionMark

--- src/utils/url.test.ts
+++ src/utils/url.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from 'vitest';
 
-import { withTrailingSlash } from './url';
+import { hasTrailingQuestionMark, withTrailingSlash } from './url';
 
 describe('url utilities', async () => {
   test('withTrailingSlash', async () => {
@@ -10,5 +10,18 @@ describe('url utilities', async () => {
     expect(withTrailingSlash('hi/there/')).toBe('hi/there/');
     expect(withTrailingSlash('hi/there/123')).toBe('hi/there/123/');
     expect(withTrailingSlash('hi/there/123/')).toBe('hi/there/123/');
+  });
+
+  test('hasTrailingQuestionMark', async () => {
+    expect(hasTrailingQuestionMark('hi')).toBe(false);
+    expect(hasTrailingQuestionMark('hi?')).toBe(true);
+    expect(hasTrailingQuestionMark('hi/there')).toBe(false);
+    expect(hasTrailingQuestionMark('hi/there?')).toBe(true);
+    expect(hasTrailingQuestionMark('hi/there/?')).toBe(true);
+    expect(hasTrailingQuestionMark('?hi/there/')).toBe(false);
+    expect(hasTrailingQuestionMark('?hi/there/?')).toBe(true);
+    expect(hasTrailingQuestionMark('hi/there/?/')).toBe(false);
+    expect(hasTrailingQuestionMark('hi/there/?id=')).toBe(false);
+    expect(hasTrailingQuestionMark('hi/there/?id=123')).toBe(false);
   });
 });
