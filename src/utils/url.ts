823e10c7922c7c0ea574af367a0302162870ec20
fatherbrennan <bjt.lepore@gmail.com>
2025-04-03 17:22:34 +1030

    chore: :white_check_mark: add hasTrailingQuestionMark

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,5 +1,9 @@
 type EnsureTrailingSlash<T extends string> = T extends `${infer P}/` ? `${P}/` : `${T}/`;
 
+export const hasTrailingQuestionMark = <T extends string>(path: T): boolean => {
+  return /\?$/.test(path);
+};
+
 export const withTrailingSlash = <T extends string>(path: T): EnsureTrailingSlash<T> => {
   return /\/$/.test(path) ? (path as unknown as EnsureTrailingSlash<T>) : (`${path}/` as EnsureTrailingSlash<T>);
 };
